ce9f87d926d3429bea3ba908016cac4718ad2ba9
NikotheCheetah <NikotheCheetah@github.com>
2021-01-28 10:31:58 -0500

    java change

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+console.log("Your index.js file is loaded correctly!");
+
+$("a").on("click", function(){
+    $(".fadeMe").fadeToggle("slow")
+}
+
+$(".linkedinButton").on("click", function(){
+    $(".fadeMe").fadeToggle("slow")
+}
+
+$(".behanceButton").on("click", function(){
+    $(".fadeMe").fadeToggle("slow")
+}
+
+$(".instagramButton").on("click", function(){
+    $(".fadeMe").fadeToggle("slow")
+}
+
